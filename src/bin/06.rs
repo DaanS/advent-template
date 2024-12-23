e2c914739b83f35b64984e59342b383d635383b6
Daan Schulpen <daanschulpen@gmail.com>
2024-12-23 01:59:48 +0100

    day 6 part 2

--- src/bin/06.rs
+++ src/bin/06.rs
@@ -1,9 +1,53 @@
+use std::ops::{Index, IndexMut};
+
 advent_of_code::solution!(6);
+
+#[derive(Clone, Copy)]
+struct Visited {
+    north: bool,
+    east: bool,
+    south: bool,
+    west: bool
+}
+
+impl Visited {
+    fn new() -> Self {
+        Visited{north: false, east: false, south: false, west: false}
+    }
+
+    fn visited_any(&self) -> bool {
+        self.north || self.east || self.south || self.west
+    }
+}
+
+impl Index<Direction> for Visited {
+    type Output = bool;
+
+    fn index(&self, index: Direction) -> &Self::Output {
+        match index {
+            Direction::North => &self.north,
+            Direction::East => &self.east,
+            Direction::South => &self.south,
+            Direction::West => &self.west
+        }
+    }
+}
+
+impl IndexMut<Direction> for Visited {
+    fn index_mut(&mut self, index: Direction) -> &mut Self::Output {
+        match index {
+            Direction::North => &mut self.north,
+            Direction::East => &mut self.east,
+            Direction::South => &mut self.south,
+            Direction::West => &mut self.west
+        }
+    }
+}
 
 #[derive(Clone, Copy)]
 enum Tile {
     Blocked,
-    Free(bool)
+    Free(Visited)
 }
 
 #[derive(Clone, Copy)]
@@ -28,8 +72,11 @@ fn parse_input(input: &str) -> ((isize, isize), Direction, Vec<Vec<Tile>>) {
             if map.len() <= x { map.push(Vec::new()); }
             map[x].push(match tile {
                 '#' => Tile::Blocked,
-                '.' => Tile::Free(false),
-                '^' => { guard_pos = (x as isize, y as isize); Tile::Free(true) }
+                '.' => Tile::Free(Visited::new()),
+                '^' => { 
+                    guard_pos = (x as isize, y as isize); 
+                    Tile::Free(Visited{north: true, east: false, south: false, west: false})
+                }
                 _ => panic!("unexpected tile {tile}")
             });
             x += 1;
@@ -49,16 +96,16 @@ fn get_next_pos((x, y): (isize, isize), dir: Direction) -> (isize, isize) {
     }
 }
 
-pub fn part_one(input: &str) -> Option<u64> {
-    let ((mut guard_pos), mut dir, mut map) = parse_input(input);
-
+fn guard_path(mut guard_pos: (isize, isize), mut dir: Direction, map: &mut Vec<Vec<Tile>>) -> bool {
     loop {
         let (new_x, new_y) = get_next_pos(guard_pos, dir);
-        if (new_x < 0) || (new_y < 0) || (new_x >= map.len() as isize) || (new_y >= map[new_x as usize].len() as isize) { break; }
+        if (new_x < 0) || (new_y < 0) || (new_x >= map.len() as isize) || (new_y >= map[new_x as usize].len() as isize) { return false; }
 
         match map[new_x as usize][new_y as usize] {
-            Tile::Free(_) => {
-                map[new_x as usize][new_y as usize] = Tile::Free(true);
+            Tile::Free(mut visited) => {
+                if visited[dir] { return true; }
+                visited[dir] = true;
+                map[new_x as usize][new_y as usize] = Tile::Free(visited);
                 guard_pos = (new_x, new_y);
             },
             Tile::Blocked => {
@@ -71,18 +118,43 @@ pub fn part_one(input: &str) -> Option<u64> {
             }
         }
     }
+}
+
+pub fn part_one(input: &str) -> Option<u64> {
+    let (guard_pos, dir, mut map) = parse_input(input);
+
+    guard_path(guard_pos, dir, &mut map);
 
     let mut count = 0;
     for line in map {
         for tile in line {
-            if let Tile::Free(true) = tile { count += 1; }
+            if let Tile::Free(visited) = tile { 
+                if visited.visited_any() { count += 1; }
+            }
         }
     }
     Some(count)
 }
 
 pub fn part_two(input: &str) -> Option<u64> {
-    None
+    let (guard_pos, dir, map_orig) = parse_input(input);
+
+    let mut count = 0;
+
+    for x in 0..map_orig.len() {
+        for y in 0..map_orig[x].len() {
+            let mut map = map_orig.clone();
+            match map[x][y] {
+                Tile::Blocked => continue,
+                Tile::Free(_) => {
+                    map[x][y] = Tile::Blocked;
+                    if guard_path(guard_pos, dir, &mut map) { count += 1; }
+                }
+            }
+        }
+    }
+    
+    Some(count)
 }
 
 #[cfg(test)]
@@ -98,6 +170,6 @@ mod tests {
     #[test]
     fn test_part_two() {
         let result = part_two(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        assert_eq!(result, Some(6));
     }
 }
